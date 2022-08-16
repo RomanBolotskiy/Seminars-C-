4219ad46677a79f5f7aae960055d858304b1233b
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-16 22:00:04 +0300

    Задача 2

---
+++ Exemple002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите 2 числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 % num2 ==0)
+{
+    Console.WriteLine("Число 2 кратно числу 1");
+}
+else
+{
+    Console.WriteLine($"Число 1 не кратно числу 2, остаток от деления равен {num1 % num2}");
+}
