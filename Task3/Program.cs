209a524f12257b7fb8bbd8830f9d60f597a3481e
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-17 16:17:53 +0300

    Определяем день недели

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите число от 1 до 7");
+int dayNumber = int.Parse (Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine ("Это выходной день");
+    }
+    else
+    {
+        Console.WriteLine ("Это не выходной день");
+    }
+}
+CheckingTheDayOfTheWeek (dayNumber);
+
