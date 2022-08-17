fb17233b1140887bd1c637f50f700054cd66141c
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-17 11:27:49 +0300

    Нашли третье число, но не могу понять как вывести ,нет третьей цифры, получается ,0,

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+int num1 = num % 10;
+int num3 = num / 100;
+
+int Result = num3 / 10 + num1;
+
+Console.WriteLine($"Число{Result} из {num}");
+*/
+
+Console.WriteLine("Введите число");
+int a = int.Parse (Console.ReadLine());
+
+int Result = (a / 100) % 10;
+
+if (a % 100 == 0)
+{
+    Console.WriteLine ("Нет третьей цифры");
+}
+else
+{
+    Console.WriteLine ($"Цифра {Result}");
+}
